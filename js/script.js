8a229975b556523c390008e38de05ede6dcdec2a
Shany Lev <shany.gariby@gmail.com>
2012-08-03 21:34:21 +0300

    Initial style

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 
 */
 
-
-
+$(function() {
+	var strArtGirl = '<article class="art-girl"><img src="/" /></article>';
+	
+	$('#surprise').click(function() {
+		var imgId = Math.ceil(Math.random() * 5), artGirl;
+		
+		artGirl = strArtGirl.replace('src="/"', 'src=img/shany' + imgId + '.JPG');
+		$('#main').append($(artGirl));		
+	});
+});
 
 
