78e6526695eaa036894f98588cc3662142ba449e
Shany Lev <shany.gariby@gmail.com>
2012-08-04 19:35:48 +0300

    Apply image lazy loading on index' feeds

--- js/script.js
+++ js/script.js
@@ -3,14 +3,37 @@
 */
 
 $(function() {
-	var girlTemplate = $('#girlListItem').html();
+	var girlTemplate = $('#girlListItem').html(),
+			eventHandlres = {
+				onSurpriseClick: function() {
+					var imgId = Math.ceil(Math.random() * 14), 
+							fileName = 'img' + imgId + '.JPG';
+					
+					$("#main").append(_.template(girlTemplate, { fileName: fileName }));				
+				}
+			};
 	
-	$('#surprise').click(function() {
-		var imgId = Math.ceil(Math.random() * 14), 
-				fileName = 'img' + imgId + '.JPG';
+	function init() {
+		var imgNum = 20;
+		for (var i=0; i<imgNum; i++) {
+					var imgId = Math.ceil(Math.random() * 14), 
+							fileName = 'img' + imgId + '.JPG';
+					
+					$("#main").append(_.template(girlTemplate, { fileName: fileName }));				
+		}
+		$('.noten').first().addClass('selected');
+		$('.no-noten:eq(1)').addClass('selected');
 		
-		$("#main").append(_.template(girlTemplate, { fileName: fileName }));
-	});
+		$('.lazy').lazyload({ effect : "fadeIn" });
+		bindEvents();
+	}
+	
+	function bindEvents() {
+		$('#surprise').click(function() { eventHandlres.onSurpriseClick.call(this);	});
+	}
+	
+	init();	
+	
 });
 
 
